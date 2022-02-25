1387e401a0b2bc47b718780c6861be21bdc988bc
Elena Uleia <E.Uleia@student.tudelft.nl>
2022-02-25 14:17:06 +0100

    Edit the code to match check-style

--- server/src/main/java/server/api/PersonController.java
+++ server/src/main/java/server/api/PersonController.java
@@ -11,80 +11,78 @@ import org.springframework.web.bind.annotation.*;
 import commons.Person;
 import server.database.PersonRepository;
 
+@RestController
+@RequestMapping("/api/person")
 
+public class PersonController {
 
-    @RestController
-    @RequestMapping("/api/person")
+    private final Random random;
+    private final PersonRepository repo;
 
-    public class PersonController {
-
-        private final Random random;
-        private final PersonRepository repo;
-
-        public PersonController(Random random, PersonRepository repo) {
-            this.random = random;
-            this.repo = repo;
-        }
-
-        @GetMapping(path = "get")
-        public List<Person> getAll() {
-            return repo.findAll();
-        }
-
-        @GetMapping("{id}")
-        public ResponseEntity<Person> getById(@PathVariable("id") long id) {
-            if (id < 0 || !repo.existsById(id)) {
-                return ResponseEntity.badRequest().build();
-            }
-            return ResponseEntity.ok(repo.getById(id));
-        }
-
-        @PostMapping(path = "post")
-        public ResponseEntity<Person> add(@RequestBody Person person) {
-
-            if (isNullOrEmpty(person.firstName) || isNullOrEmpty(person.lastName)) {
-                return ResponseEntity.badRequest().build();
-            }
-
-            Person saved = repo.save(person);
-            return ResponseEntity.ok(saved);
-        }
-
-
-
-        private static boolean isNullOrEmpty(String s) {
-            return s == null || s.isEmpty();
-        }
-
-        @GetMapping(path = "get/rnd")
-        public ResponseEntity<Person> getRandom() {
-            var idx = random.nextInt((int) repo.count());
-            return ResponseEntity.ok(repo.getById((long) idx));
-        }
-
-
-        @DeleteMapping("delete/{id}")
-        public ResponseEntity<Person> deleteById(@PathVariable("id") long id ){
-            repo.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
-
-        @PutMapping("put/{id}")
-        public Person updateById(@RequestBody Person newPerson, @PathVariable("id") long id) {
-
-            return repo.findById(id)
-                    .map(person -> {
-                        person.setFirstName(newPerson.getFirstName());
-                        person.setLastName(newPerson.getLastName());
-                        return repo.save(person);
-                    })
-                    .orElseGet(() -> {
-                        newPerson.setId(id);
-                        return repo.save(newPerson);
-                    });
-        }
-
-
-
-
+    public PersonController(Random random, PersonRepository repo) {
+        this.random = random;
+        this.repo = repo;
     }
+
+    @GetMapping(path = "get")
+    public List<Person> getAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Person> getById(@PathVariable("id") long id) {
+        if (id < 0 || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.getById(id));
+    }
+
+    @PostMapping(path = "post")
+    public ResponseEntity<Person> add(@RequestBody Person person) {
+
+        if (isNullOrEmpty(person.firstName) || isNullOrEmpty(person.lastName)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Person saved = repo.save(person);
+        return ResponseEntity.ok(saved);
+    }
+
+
+
+    private static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
+    @GetMapping(path = "get/rnd")
+    public ResponseEntity<Person> getRandom() {
+        var idx = random.nextInt((int) repo.count());
+        return ResponseEntity.ok(repo.getById((long) idx));
+    }
+
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Person> deleteById(@PathVariable("id") long id ){
+        repo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("put/{id}")
+    public Person updateById(@RequestBody Person newPerson, @PathVariable("id") long id) {
+
+        return repo.findById(id)
+                .map(person -> {
+                    person.setFirstName(newPerson.getFirstName());
+                    person.setLastName(newPerson.getLastName());
+                    return repo.save(person);
+                })
+                .orElseGet(() -> {
+                    newPerson.setId(id);
+                    return repo.save(newPerson);
+                });
+    }
+
+
+
+
+}
