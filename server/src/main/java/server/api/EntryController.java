292ea03e383f876cf5e4bc72ea0dcfae3c198661
Franciszek Latała <F.J.Latala@student.tudelft.nl>
2022-03-06 12:14:57 +0100

    fixing bugs

--- server/src/main/java/server/api/EntryController.java
+++ server/src/main/java/server/api/EntryController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 
-import commons.Person;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
