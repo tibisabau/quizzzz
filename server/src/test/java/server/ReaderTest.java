f1591a79f46ab290f8e11c24ab5670ec82cc6f83
Franciszek Latała <F.J.Latala@student.tudelft.nl>
2022-03-17 13:08:27 +0100

    fixing pipeline

--- server/src/test/java/server/ReaderTest.java
+++ server/src/test/java/server/ReaderTest.java
@@ -9,12 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReaderTest {
     ActivityParse activityParse;
+
     Activity activity;
+
     @BeforeEach
     void setUp() {
-        activityParse = new ActivityParse("id","path","title",1000,"source");
+        activityParse = new ActivityParse("id","path",
+                "title",1000,"source");
         activity = new Activity("path","title",1000);
     }
+
     @Test
     void convert() {
         Activity testActivity = Reader.convert(activityParse);
