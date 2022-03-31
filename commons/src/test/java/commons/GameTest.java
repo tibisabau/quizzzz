317b66bdcdc2d7268784efebdc29773c642254d6
Franciszek Latała <F.J.Latala@student.tudelft.nl>
2022-03-31 13:57:43 +0200

    adding test for Game class

---
+++ commons/src/test/java/commons/GameTest.java
@@ -0,0 +1,73 @@
+package commons;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    Game game;
+
+    Object o1;
+
+    Object o2;
+
+    Score score;
+
+    @BeforeEach
+    void setUp(){
+
+    List<Object> questions = new ArrayList<>();
+
+    o1 = new Object();
+
+    o2 = new Object();
+
+    score = new Score("user",1000);
+
+    questions.add(o1);
+    questions.add(o2);
+
+    game = new Game(20, questions);
+
+    game.updateScore(score);
+    }
+
+    @Test
+    void getCounter() {
+        assertEquals(0, game.getCounter());
+    }
+
+    @Test
+    void getUser() {
+        assertEquals(score, game.getUser());
+    }
+
+    @Test
+    void updateScore() {
+        Score testScore = new Score("test",20000);
+        game.updateScore(testScore);
+        assertEquals(testScore, game.getUser());
+    }
+
+    @Test
+    void incrementScore() {
+        game.incrementScore(50);
+        assertEquals(1050, game.getUser().getScore());
+    }
+
+    @Test
+    void getNextQuestion() {
+        assertEquals(o1, game.getNextQuestion());
+    }
+
+    @Test
+    void getID() {
+        assertEquals(20, game.getID());
+    }
+}
---
+++ commons/src/test/java/commons/InsteadOfQuestionTest.java
@@ -0,0 +1,64 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsteadOfQuestionTest {
+
+    @Test
+    void getPromptedOption() {
+    }
+
+    @Test
+    void getIdentity() {
+    }
+
+    @Test
+    void setPromptedOption() {
+    }
+
+    @Test
+    void getCorrectOption() {
+    }
+
+    @Test
+    void setCorrectOption() {
+    }
+
+    @Test
+    void getFirstOption() {
+    }
+
+    @Test
+    void setFirstOption() {
+    }
+
+    @Test
+    void getSecondOption() {
+    }
+
+    @Test
+    void getThirdOption() {
+    }
+
+    @Test
+    void setSecondOption() {
+    }
+
+    @Test
+    void setThirdOption() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
